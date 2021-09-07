b65345a9d5be1e07491d0b49b08aa9fb02d0eca0
RafikBoubaker <boubaker.rafik@gmail.com>
2021-09-07 15:33:38 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View ,Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
+   const [secondText , setSecondText] = useState('')
+
+  const InputHandler=(enteredText) => {
+    setSecondText(enteredText)
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Text>{secondText}</Text>
+      <TextInput placeholder="type your text in here" style={{ borderColor: 'black' }}
+        onChangeText={InputHandler }/>
+      <Button title ='change text' onPress={()=>{setOutputText('text changed ! ')}} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,6 +25,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
